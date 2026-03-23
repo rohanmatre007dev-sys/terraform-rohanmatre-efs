77ac236b9d3b5f0ed197a0db7fb01b93b86cc796
Rohan Matre <rohan.matre@iqinfinite.in>
2026-03-23 16:37:50 +0530

    Fix: Region is default

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "create" {
 variable "region" {
   description = "AWS region where EFS will be created"
   type        = string
-  default     = "ap-south-1"
+  # default     = "ap-south-1"
 }
 
 variable "environment" {
